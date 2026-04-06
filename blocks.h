540fe8cb0dd5796ee9e4cfe10b31cd7a141218b0
swaminsane <swami13vivekanand@gmail.com>
2026-04-06 08:04:19 +0530

    changes

--- blocks.h
+++ blocks.h
@@ -1,11 +1,8 @@
 static const Block blocks[] = {
-	{"", "sb-cpu", 20, 18},
-	{"", "sb-memory", 30, 14},
-	{"", "sb-battery", 30, 3},
-	{"", "sb-brightness", 5, 11},
-	{"", "sb-internet", 5, 4},
-	{"", "sb-volume", 1, 10},
-	{"", "sb-clock", 5, 0},
+    {"", "sb-music", 5, 1},       {"", "sb-cpu", 20, 18},
+    {"", "sb-memory", 20, 14},    {"", "sb-battery", 20, 3},
+    {"", "sb-brightness", 5, 11}, {"", "sb-internet", 5, 4},
+    {"", "sb-volume", 1, 10},     {"", "sb-clock", 1, 0},
 };
 
 static char delim[] = " | ";
